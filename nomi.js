fde43f36f83b16e5ece0781de5a788f2a1f2eb46
parthsrivastava100 <58399016+parthsrivastava100@users.noreply.github.com>
2020-03-04 12:13:26 +0530

    Update nomi.js

--- nomi.js
+++ nomi.js
@@ -5,21 +5,46 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const MongoClient = require('mongodb').MongoClient
-  MongoClient.connect('your-mongodb-url', (err, client) => {
-   if(err) return console.log('Error')
-   db = client.db('star-wars') // whatever your database name is
+  MongoClient.connect('mongodb+srv://parth:<GONDOLAS>@nom-qqkjg.mongodb.net/test?retryWrites=true&w=majority', (err, client) => {
+   if(err) return console.log('Error in connecting')
+  dbo=db.db('students')
+   db = client.db('students') // whatever your database name is
   })
-
-  app.post('/ratify', urlencodedParser, function (req, res) {
-    var query={ status:false}
-    db.collection("<Your Database Name><").find(query, { projection: { uname:0,phno:0,ratifs{}.code:0 } }).toArray(function(err, result) {
+   app.post('/ratify', urlencodedParser, function (req, res) {
+    var query={ dbo.IITK.aggregate([
+      {
+         $project: {
+            ratifs: {
+               $filter: {
+                  input: "$items",
+                  as: "item",
+                  cond: { $gte: [ "$$status", 'false' ] }
+               }
+            }
+         }
+      }
+   ])}
+    dbo.IITK("<students><").find(query, { projection: { uname:0,phno:0 } }).toArray(function(err, result) {           //MAYBE AN ERROR LINE
         if (err) throw err;
         res.end(JSON.stringify(result));
  })
+ 
  app.post('/ratify_club',urlencodedParser , function(req , res) {
     var name=req.query.club_name
-     query = { body: name , status:false };
-    db.collection("<y}our database name></y}our>").find(query).toArray(function(err, result) {
+    var query={ dbo.IITK.aggregate([
+      {
+         $project: {
+            ratifs: {
+               $filter: {
+                  input: "$items",
+                  as: "item",
+                  cond: { $gte:[ [ "$$status", 'false' ],["$$name",name]] }
+               }
+            }
+         }
+      }
+   ])}
+    db.IITK("<y}our database name></y}our>").find(query).toArray(function(err, result) {
         if (err) throw err;
         res.end(JSON.stringify(result));   
 
@@ -27,14 +52,26 @@ const MongoClient = require('mongodb').MongoClient
  app.post('/update',urlencodedParser , function(req , res){
   var nam=req.body.name
   var code=req.body.code
-  var stat=req.body.status
-  var myquery={name:nam,ratify{}.code:code,ratify{}.status:stat}
-  var newvalues = { $set: { ratify{}.status: stat } };
-  db.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+  var stats=req.body.status
+  var query={ dbo.IITK.aggregate([
+   {
+      $project: {
+         ratifs: {
+            $filter: {
+               input: "$items",
+               as: "item",
+               cond: { $gte:[ [ "$$status", 'stats' ],["$$name",name],["$$code",code]] }
+            }
+         }
+      }
+   }
+])}
+  var newvalues = { $set: { ratifs[].status: stat } };
+  db.IITK("customers").updateOne(query, newvalues, function(err, res) {
       if(err) console.log('Error')
-  }
+  })
 
- })
+   })
 
  var server = app.listen(8081, function () {     // ENTER THE DESIRED PORT NUMBER
     var host = server.address().address
